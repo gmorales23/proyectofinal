fcc11800fabcdc9ac8ddde7a6e14dc0703eba1b5
ezemz97 <eze.mz@outlook.com>
2025-09-08 20:44:07 -0300

    Agregar filtros (visual) en la pagina de productos.
    Agregar codigo para ordenar ascendientemente y descendientemente por precios de productos
    
    Co-authored-by: gmorales23 <gmorales23@users.noreply.github.com>
    Co-authored-by: Angela-Echevarria <Angela-Echevarria@users.noreply.github.com>
    Co-authored-by: Bernardo Collins <bernardocollins6@users.noreply.github.com>
    Co-authored-by: Florencia <florenciaolhagaray@users.noreply.github.com>

--- js/products.js
+++ js/products.js
@@ -4,20 +4,17 @@ let url = `https://japceibal.github.io/emercado-api/cats_products/${catID}.json`
 const contenedor = document.getElementById("catalogo");
 const tituloCategoria = document.getElementById("titulo-categoria");
 
-{
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const nombreCategoria = data.catName;
-            tituloCategoria.textContent = nombreCategoria;
-            document.title = `eMercado - ${nombreCategoria}`;
+const ordenarAsc = document.getElementById("sortAsc");
+const ordenarDes = document.getElementById("sortDesc");
+const ordenarRel = document.getElementById("sortByCount");
 
-            const productos = data.products;
+let productosOriginales = {};
 
-            let htmlContent = "";
 
-            productos.forEach((producto) => {
-                htmlContent += `
+function mostrarProductos(productos) {
+    let htmlContent = "";
+    productos.forEach((producto) => {
+        htmlContent += `
             <div class="col-12 col-md-6 col-lg-4">
                 <div class="card h-100 custom-shadow">
                     <img src="${producto.image}" class="card-img-top" alt="${producto.name}">
@@ -31,9 +28,23 @@ const tituloCategoria = document.getElementById("titulo-categoria");
                     </div>
                 </div>
             </div>`;
-            });
+    });
+    contenedor.innerHTML = htmlContent;
+}
 
-            contenedor.innerHTML = htmlContent;
+{
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const nombreCategoria = data.catName;
+            tituloCategoria.textContent = nombreCategoria;
+            document.title = `eMercado - ${nombreCategoria}`;
+
+            const productos = data.products;
+
+            mostrarProductos(productos);
+
+            productosOriginales = productos;
         })
         .catch((error) => {
             contenedor.innerHTML = `
@@ -46,3 +57,15 @@ const tituloCategoria = document.getElementById("titulo-categoria");
 window.addEventListener("DOMContentLoaded", () => {
     mostrarUsuarioLogueado("#userNav", false);
 });
+
+ordenarAsc.addEventListener("click", () => {
+    const productosOrdenados = [...productosOriginales].sort((a, b) => b.cost - a.cost);
+    mostrarProductos(productosOrdenados);
+});
+
+ordenarDes.addEventListener("click", () => {
+    const productosOrdenados = [...productosOriginales].sort((a, b) => a.cost - b.cost);
+    mostrarProductos(productosOrdenados);
+});
+
+
